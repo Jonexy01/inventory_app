b84fd5ee07b2043446a0b69cf881e910aa5d2582
Jonexy01 <jonexy01@gmail.com>
2022-03-01 13:48:25 +0100

    building manager status page

--- lib/pages/AuthenticationPages/manager_status.dart
+++ lib/pages/AuthenticationPages/manager_status.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../../MyWidgets/my_rounded_input_field.dart';
+
 class ManagerStatus extends StatefulWidget {
   const ManagerStatus({ Key? key }) : super(key: key);
 
@@ -11,6 +13,11 @@ class _ManagerStatusState extends State<ManagerStatus> {
 
   final _formKey = GlobalKey<FormState>();
 
+  //text field state
+  String name = '';
+  String businessName = '';
+  String error = '';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,7 +27,22 @@ class _ManagerStatusState extends State<ManagerStatus> {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              
+              Text('Provide your details'),
+              Spacer(),
+              MyRoundedInputField(
+                onChanged: ((value) {
+                  name = value;
+                }),
+                hintText: 'Enter your name',
+                icon: Icons.person,
+              ),
+              SizedBox(height: 10,),
+              MyRoundedInputField(
+                onChanged: ((value) {
+                  businessName = value;
+                }),
+                hintText: 'Enter your business name',
+              ),
             ],
           ),
         )
